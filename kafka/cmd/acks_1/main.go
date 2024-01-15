75d58aa45cda1528ab53b737e43de46b2b0fb173
artur.yumaev <artur.yumaev@sbermarket.ru>
2024-01-15 21:00:03 +0300

    Added local kafka env

---
+++ kafka/cmd/acks_1/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+
+	"github.com/IBM/sarama"
+)
+
+var (
+	maxMessages = 2_000
+	successes   int32
+	errors      int32
+)
+
+func main() {
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = 1
+	config.Producer.Return.Successes = true
+
+	producer, err := sarama.NewAsyncProducer([]string{"localhost:9092"}, config)
+	if err != nil {
+		panic(err)
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		for range producer.Successes() {
+			atomic.AddInt32(&successes, 1)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for err := range producer.Errors() {
+			atomic.AddInt32(&errors, 1)
+			fmt.Println(err)
+		}
+	}()
+
+	for i := 1; i <= maxMessages; i++ {
+		message := sarama.ProducerMessage{Topic: "test", Value: sarama.StringEncoder("testing 123")}
+		producer.Input() <- &message
+
+		if i > 0 && i%100 == 0 {
+			fmt.Printf("wrote %d messages\n", i)
+		}
+	}
+	go producer.AsyncClose()
+
+	wg.Wait()
+
+	fmt.Printf("Stats: successes: %d, errors: %d\n", successes, errors)
+}
