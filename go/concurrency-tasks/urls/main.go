76c95d74b05bf618fe46abcb4318b932cac4e091
artur.yumaev <artur.yumaev@sbermarket.ru>
2024-02-05 19:55:35 +0300

    Added limited fan-out

--- go/concurrency-tasks/urls/main.go
+++ go/concurrency-tasks/urls/main.go
@@ -68,6 +68,39 @@ func fanOut() {
 	}
 }
 
+func limitedFanOut() {
+	simGoroutines := 3
+	blockChan := make(chan struct{}, simGoroutines)
+	defer close(blockChan)
+	errs := make(chan error)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(len(urls))
+
+	go func() {
+		wg.Wait()
+		close(errs)
+	}()
+
+	for _, url := range urls {
+		url := url
+
+		go func() {
+			blockChan <- struct{}{}
+			defer wg.Done()
+
+			err := healthCheck(url)
+			errs <- err
+
+			<-blockChan
+		}()
+	}
+
+	for err := range errs {
+		fmt.Println(err)
+	}
+}
+
 func workerPool() {
 	N := runtime.NumCPU()
 	in := make(chan string)
@@ -104,7 +137,10 @@ func workerPool() {
 }
 
 func main() {
+	fmt.Println("fan-out")
 	fanOut()
-	fmt.Println("----------")
+	fmt.Println("limited fan-out")
+	limitedFanOut()
+	fmt.Println("worker pool")
 	workerPool()
 }
