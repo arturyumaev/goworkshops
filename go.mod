bedcd02340350dfa83286a8e3a11e9d870191484
artur.yumaev <artur.yumaev@sbermarket.ru>
2024-02-05 19:28:55 +0300

    Added interview task on concurrency

--- go.mod
+++ go.mod
@@ -1,9 +1,10 @@
-module github.com/arturyumaev/goworkshops/kafka
+module github.com/arturyumaev/goworkshops
 
 go 1.21.1
 
+require github.com/IBM/sarama v1.42.1
+
 require (
-	github.com/IBM/sarama v1.42.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/eapache/go-resiliency v1.4.0 // indirect
 	github.com/eapache/go-xerial-snappy v0.0.0-20230731223053-c322873962e3 // indirect
---
+++ go/concurrency-tasks/urls/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"sync"
+	"time"
+)
+
+var (
+	urls = []string{
+		"http://node-1:3000",
+		"http://node-2:3000",
+		"http://node-3:3000",
+		"http://node-4:3000",
+		"http://node-5:3000",
+	}
+)
+
+func healthCheck(url string) error {
+	url += "/healthcheck"
+	time.Sleep(time.Duration(rand.Intn(2000)) * time.Millisecond)
+	return nil
+}
+
+/*
+// Example
+
+func healthCheck(url string) error {
+	url += "/healthcheck"
+	time.Sleep(time.Second)
+	return nil
+}
+
+func main() {
+	for _, url := range urls {
+		err := healthCheck(url)
+		fmt.Println(err)
+	}
+}
+*/
+
+func fanOut() {
+	errs := make(chan error)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(len(urls))
+
+	go func() {
+		wg.Wait()
+		close(errs)
+	}()
+
+	for _, url := range urls {
+		url := url
+
+		go func() {
+			defer wg.Done()
+
+			err := healthCheck(url)
+			errs <- err
+		}()
+	}
+
+	for err := range errs {
+		fmt.Println(err)
+	}
+}
+
+func workerPool() {
+	N := runtime.NumCPU()
+	in := make(chan string)
+	errs := make(chan error, len(urls))
+
+	wg := &sync.WaitGroup{}
+	wg.Add(len(urls))
+
+	go func() {
+		wg.Wait()
+		close(errs)
+	}()
+
+	go func() {
+		for _, url := range urls {
+			in <- url
+		}
+		close(in)
+	}()
+
+	for i := 0; i < N; i++ {
+		go func() {
+			for url := range in {
+				err := healthCheck(url)
+				errs <- err
+				wg.Done()
+			}
+		}()
+	}
+
+	for err := range errs {
+		fmt.Println(err)
+	}
+}
+
+func main() {
+	fanOut()
+	fmt.Println("----------")
+	workerPool()
+}
